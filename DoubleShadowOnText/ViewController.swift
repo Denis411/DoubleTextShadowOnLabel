9944a7a4bde0448f3af0b83a296d570c76ba6019
FIX PRICE <RETAIL@Deniss-MacBook-Pro.local>
2024-07-02 17:29:35 +0700

    Add basic UI

--- DoubleShadowOnText/ViewController.swift
+++ DoubleShadowOnText/ViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class LabelWithShadow: UILabel {
+    
+}
 
+final class ViewController: UIViewController {
+    private let label: LabelWithShadow = {
+        let label = LabelWithShadow()
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 40, weight: .semibold)
+        label.text = "Новинки"
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor(named: "BackgroundColor")
+        view.addSubview(label)
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        label.frame.size = label.intrinsicContentSize
+        label.center = view.center
+    }
 
 }
 
