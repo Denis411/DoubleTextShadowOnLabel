5fe05f198a213dd04c4f22bf3e23f2bb01769c4a
FIX PRICE <RETAIL@Deniss-MacBook-Pro.local>
2024-07-05 14:36:53 +0700

    Add shadow

--- DoubleShadowOnText/ViewController.swift
+++ DoubleShadowOnText/ViewController.swift
@@ -6,9 +6,73 @@
 //
 
 import UIKit
+import CoreImage.CIFilterBuiltins
 
 final class LabelWithShadow: UILabel {
+    let firstShadowLayer = CATextLayer()
+    let secondShadowLayer = CATextLayer()
     
+    let firstShadowBlur = CIFilter.bokehBlur()
+    let secondShadowBlur = CIFilter.bokehBlur()
+    
+    override var text: String? {
+        didSet {
+            firstShadowLayer.string = text
+            secondShadowLayer.string = text
+        }
+    }
+    
+    override var font: UIFont! {
+        didSet {
+            firstShadowLayer.font = font
+            secondShadowLayer.font = font
+            firstShadowLayer.fontSize = 40
+            secondShadowLayer.fontSize = 40
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.layer.masksToBounds = false
+        firstShadowLayer.masksToBounds = false
+        secondShadowLayer.masksToBounds = false
+        layer.addSublayer(firstShadowLayer)
+        layer.addSublayer(secondShadowLayer)
+        
+        firstShadowLayer.shadowOffset = CGSize(width: 0, height: 2)
+        firstShadowLayer.shadowColor = Self.firstShadowDefaultColor.cgColor
+        firstShadowLayer.shadowOpacity = 0.5
+        
+        firstShadowBlur.radius = 2.0
+        firstShadowLayer.filters = [firstShadowBlur]
+        
+        secondShadowLayer.shadowOffset = CGSize(width: 0, height: 2)
+        secondShadowLayer.shadowColor = Self.secondShadowDefaultColor.cgColor
+        secondShadowLayer.shadowOpacity = 0.2
+        
+        secondShadowBlur.radius = 16.0
+        secondShadowLayer.filters = [secondShadowBlur]
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        firstShadowLayer.frame = self.bounds
+        secondShadowLayer.frame = self.bounds
+    }
+    
+    func setShadowColor(color: UIColor) {
+        firstShadowLayer.shadowColor = color.cgColor
+        secondShadowLayer.shadowColor = color.cgColor
+    }
+}
+
+private extension LabelWithShadow {
+    static let firstShadowDefaultColor = UIColor(red: 0, green: 0, blue: 85, alpha: 1)
+    static let secondShadowDefaultColor = UIColor(red: 0, green: 0, blue: 119, alpha: 1)
 }
 
 final class ViewController: UIViewController {
